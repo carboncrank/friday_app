eda1b7e279980bfdb97f8f801b4fa48a3a889010
Carbon Crank <johnmforsyth@yahoo.co.uk>
2012-03-16 12:24:47 +0000

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
+    @random = "random"
+  end
+
+  def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
   end
 end
