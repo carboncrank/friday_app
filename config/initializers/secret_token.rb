41fee90e8ffee8fbc80fbac07b439379195d1c1c
Carbon Crank <johnmforsyth@yahoo.co.uk>
2012-03-16 11:29:52 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FridayApp::Application.config.secret_token = '02762c0d9671ebe62670db19468ea9c70a132222a450df863d062b4edf8b5ba4cd6bdc6d6c57a094299cfa7833e847558ebe5c445acf9ea815e53fbd3b09aca3'
