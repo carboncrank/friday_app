426fa327f6331d920208152c491cf5d250495d36
Carbon Crank <johnmforsyth@yahoo.co.uk>
2012-03-20 18:49:25 +0000

    Added a User factory and tests for the show page

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :test do
 	gem 'rspec'
 	gem 'webrat'
 	gem 'spork'
+	gem 'factory_girl_rails', '1.0'
 end
 
 # Gems used only for assets and not required
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "John Forsyth"
+  user.email                 "john@example.org"
+  user.password              "foobar"
+  user.password_confirmation "foobar"  
+end
