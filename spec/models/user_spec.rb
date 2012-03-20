c0b15d0395b163ad864bc193ab1027f567b331fa
Carbon Crank <johnmforsyth@yahoo.co.uk>
2012-03-20 17:34:54 +0000

    Added encrypted_password attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -96,7 +96,19 @@ describe User do
       long = "a" * 41
       hash = @attr.merge(:password => long, :password_confirmation => long)
       User.new(hash).should_not be_valid
+    end    
+  end
+  
+  describe "password encryption" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
     end
     
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
   end
+  
+  
 end
